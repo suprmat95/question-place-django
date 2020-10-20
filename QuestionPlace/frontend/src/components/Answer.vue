e6cae75a1a2751f968bb9573d9ff4963a8ed7281
Matteo D'Ospina <matteodospina@gmail.com>
2020-10-20 23:31:36 +0200

    now it's possible to put likes

--- QuestionPlace/frontend/src/components/Answer.vue
+++ QuestionPlace/frontend/src/components/Answer.vue
@@ -4,7 +4,7 @@
             <strong>{{answer.author}}</strong> ha risposto il {{answer.created_at}}
         </p>
         <p>{{answer.body}}</p>
-        <div v-if="isAnswerAuthor()" class="answer-owner">
+        <div v-if="isAnswerAuthor" class="answer-owner">
             <router-link
                     :to="{name:'answer-editor', params: {id: answer.id}}"
                     class="btn btn-sm btn-outline-secondary mr-1"
@@ -18,11 +18,24 @@
                     >Cancella</button>
 
         </div>
+        <div v-else class="like-answer">
+            <button
+                    class="btn btn-sm"
+                    @click="toggleLike"
+                    :class="{
+                        'btn-danger': userLikedAnswer,
+                        'btn-outline-danger': !userLikedAnswer,
+                    }">
+                <strong>Mi piace [{{likesNumber}}]</strong>
+            </button>
+        </div>
     </div>
 
 </template>
 
 <script>
+    import {apiService} from "../common/api.service";
+
     export default {
         name: "AnswerComponent",
         props: {
@@ -35,9 +48,34 @@
                 required: true
             }
         },
-        methods: {
+        data (){
+            return{
+                userLikedAnswer: this.answer.user_has_voted,
+                likesNumber: this.answer.likes_count
+            }
+        },
+        computed: {
             isAnswerAuthor() {
                 return this.answer.author === this.requestUser;
+            }
+        },
+        methods: {
+            toggleLike() {
+                this.userLikedAnswer===false
+                    ? this.likeAnswer()
+                    : this.unLikeAnswer()
+            },
+            likeAnswer() {
+                this.userLikedAnswer =true;
+                this.likesNumber+=1;
+                let endpoint = `/api/answers/${this.answer.id}/like/`;
+                apiService(endpoint, "POST");
+            },
+            unLikeAnswer() {
+                this.userLikedAnswer =false;
+                this.likesNumber-=1;
+                let endpoint = `/api/answers/${this.answer.id}/like/`;
+                apiService(endpoint, "DELETE");
             },
             triggerDeleteAnswer() {
                 this.$emit("delete-answer",this.answer)
