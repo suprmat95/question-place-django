e090c4cd20d6bb1b2ab80502183b1efc428f8918
Matteo D'Ospina <matteodospina@gmail.com>
2020-10-20 19:05:53 +0200

    now we can add answer by form

--- QuestionPlace/frontend/src/views/Question.vue
+++ QuestionPlace/frontend/src/views/Question.vue
@@ -7,6 +7,35 @@
             </p>
             <p>{{ question.created_at }}</p>
             <hr>
+            <template v-if="userHasAnswered">
+                <p class="answer-added">Hai risposto a questa domanda.</p>
+            </template>
+            <template v-else-if="showForm">
+                <form class="card" @submit.prevent="onSubmit">
+                    <div class="card-header px-3">
+                        Aggiungi una risposta alla domanda
+                    </div>
+                    <div class="card-block">
+                        <textarea
+                                v-model="newAnswerBody"
+                                class="form-control"
+                                placeholder="Aggiungi una risposta alla domanda"
+                                rows="5"
+                                ></textarea>
+                    </div>
+                    <div class="card-footer px-3">
+                        <button type="submit" class="btn btn-sm btn-success">Aggiungi risposta</button>
+                    </div>
+                </form>
+                <p class="error mt-2">{{error}}</p>
+            </template>
+            <template v-else>
+                <button
+                        class="btn btn-sm btn-success"
+                        @click="showForm=true"
+                        >Rispondi alla Domanda</button>
+            </template>
+            <hr>
             <AnswerComponent
                 v-for="(answer, index) in answers"
                 :answer="answer"
@@ -37,7 +66,11 @@
             return {
                 question: {},
                 loadingAnswers: false,
-                answers: []
+                answers: [],
+                userHasAnswered: false,
+                showForm: false,
+                newAnswerBody: null,
+                error: null
             }
         },
 
@@ -50,6 +83,7 @@
                 apiService(endpoint)
                 .then(data =>{
                     this.question = data;
+                    this.userHasAnswered = data.user_has_answered;
                     this.setPageTitle(data.content)
                 })
             },
@@ -62,6 +96,24 @@
                     this.loadingAnswers = false;
                 })
 
+            },
+            onSubmit() {
+                if(this.newAnswerBody){
+                    let endpoint = `/api/questions/${this.slug}/answer/`;
+                    apiService(endpoint, "POST", {body: this.newAnswerBody})
+                        .then(data => {
+                            this.answers.unshift(data);
+                        })
+                    this.newAnswerBody = null;
+                    this.showForm = false;
+                    this.userHasAnswered = true;
+                    if(this.error) {
+                        this.error = null
+                    }
+                }
+                else {
+                    this.error = "Il campo non puo essere vuoto."
+                }
             }
         },
         created() {
@@ -74,5 +126,13 @@
 <style scoped>
 .single-question {
     text-align: left;
+}
+.error {
+    color: red;
+    font-weight: bold;
+}
+.answer-added {
+    color: green;
+    font-weight: bold;
 }
 </style>
